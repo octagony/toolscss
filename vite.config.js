fcd5abca6cab5428a65dbabedae7224595e03e77
octagony <moctagony@gmail.com>
2025-01-25 16:17:02 +0300

    [FEAT] Update version to 0.0.144, added Vite for buildng

---
+++ vite.config.js
@@ -0,0 +1,29 @@
+// vite.config.js
+import cssnano from 'cssnano'
+import path from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+	root: './src',
+	build: {
+		outDir: './dist',
+		rollupOptions: {
+			input: path.resolve(__dirname, 'src/index.scss'),
+			output: {
+				assetFileNames: () => {
+					return 'index.min.css'
+				},
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, 'src'),
+		},
+	},
+	css: {
+		postcss: {
+			plugins: [cssnano({ preset: 'default' })],
+		},
+	},
+})
